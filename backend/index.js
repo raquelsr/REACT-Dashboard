fb914b5f8ef538864f496e205b18a57f8d9216a6
Raquel Serrano <raquelserranor@hotmail.com>
2022-07-29 13:35:59 +0200

    feat: first routes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,37 @@
 
 const Hapi = require('@hapi/hapi');
 
+const data = {
+    users: [{
+        user: 'Raquel',
+        rol: 'Admin'
+    }]
+}
+
 const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
         host: 'localhost'
     });
+
+    server.route([{
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return data;
+        }
+    },
+    {
+        method: 'POST',
+        path: '/',
+        handler: (request, h) => {
+            data.users.push(request.payload);
+            return data;
+        }
+    }]);
+
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
