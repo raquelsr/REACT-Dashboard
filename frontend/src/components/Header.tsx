fa1a3f32bb0f1eece2d57b617d363514d4fede34
Raquel Serrano <raquelserranor@hotmail.com>
2022-08-12 17:40:41 +0200

    feat: FE - Communicate with BE

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,32 @@
+const endpoint = 'http://localhost:8000/graphql/';
+const QUERY = `
+  {
+    getUsers {
+      id
+      name
+    }
+  }
+`;
+
+
 
 export const Header = () => {
-    return <>Header</>;
+    fetch(endpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query: QUERY })
+    })
+        .then((response) => {
+            console.log(response);
+        });
+
+    return (
+        <div>
+            <h1>Header</h1>
+        </div>
+    );
 };
-  
+
+
