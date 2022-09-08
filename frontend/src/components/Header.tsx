3fe6c68eafcd1f45163df99a71e6b8d9afb849e5
Raquel Serrano <raquelserranor@hotmail.com>
2022-09-08 12:23:32 +0200

    FE: Add tailwind

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,12 @@
+import header from '../assets/mt-orange.png';
+
 export const Header = () => {
     return (
         <div>
-            <h1>Dashboard</h1>
+            <img src={header} className='w-full h-10'></img>
+            <h1 className='text-orange-400 text-4xl text-center font-bold m-4'>
+                MT Dashboard
+            </h1>
         </div>
     );
 };
