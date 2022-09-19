248ca9fa69d702e70a368e8e896824468face2b7
Raquel Serrano <raquelserranor@hotmail.com>
2022-09-19 15:34:21 +0200

    FE: Refactor

--- frontend/src/containers/Dashboard.tsx
+++ frontend/src/containers/Dashboard.tsx
@@ -4,27 +4,16 @@ import { LineChart } from '../components/LineChart';
 import { PieChart } from '../components/PieChart';
 import { RadarChart } from '../components/RadarChart';
 import { SectionTitle } from '../components/SectionTitle';
-import { Summary } from '../components/Summary';
+import { Summary } from './Summary';
 import { Table } from '../components/Table';
-// import { GET_DATA } from '../queries/data';
 import { GET_TOTAL_USERS_BY_COUNTRY, GET_TOTAL_USERS_BY_JOB_POSITION } from '../queries/user';
 
 export const Dashboard = () => {
-  // const { loading, error, data } = useQuery(GET_DATA);
-  // const { loading: loadingUsers, error: errorUsers, data: dataUsers } = useQuery(GET_USERS);
-  const { loading: loadingUsers2, error: errorUsers2, data: totalUsers } = useQuery(GET_TOTAL_USERS_BY_COUNTRY);
-  const { loading: loadingUsers3, error: errorUsers3, data: totalUsers3 } = useQuery(GET_TOTAL_USERS_BY_JOB_POSITION);
+  const { loading: loadingUsersByCountry, error: errorUsersByCountry, data: totalUsersByCountry } = useQuery(GET_TOTAL_USERS_BY_COUNTRY);
+  const { loading: loadingUsersByJobPosition, error: errorUsersByJobPosition, data: totalUsersByJobPosition } = useQuery(GET_TOTAL_USERS_BY_JOB_POSITION);
 
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Upps...There is an error. :( </p>;
-  // if (loadingUsers) return <p>Loading...</p>;
-  // if (errorUsers) return <p>Upps...There is an error. :( </p>;
-  if (loadingUsers2) return <p>Loading...</p>;
-  if (errorUsers2) return <p>Upps...There is an error. :( </p>;
-  if (loadingUsers3) return <p>Loading...</p>;
-  if (errorUsers3) return <p>Upps...There is an error. :( </p>;
-
-
+  if (loadingUsersByCountry && loadingUsersByJobPosition) return <p>Loading...</p>;
+  if (errorUsersByCountry || errorUsersByJobPosition) return <p>Upps...There is an error. :( </p>;
 
   return (
     <div>
@@ -33,14 +22,14 @@ export const Dashboard = () => {
       <Table />
       <SectionTitle title='Charts'></SectionTitle>
       <div className='flex justify-center mb-24'>
-        <BarChart data={totalUsers.getTotalUsersByCountry} dataKey='totalCount' xAxisKeys='country' layout='horizontal'></BarChart>
-        <PieChart data={totalUsers.getTotalUsersByCountry} dataKey='percentage' nameKey='country'></PieChart>
+        <BarChart data={totalUsersByCountry.getTotalUsersByCountry} dataKey='totalCount' xAxisKeys='country' layout='horizontal'></BarChart>
+        <PieChart data={totalUsersByCountry.getTotalUsersByCountry} dataKey='percentage' nameKey='country'></PieChart>
       </div>
       <div className='flex justify-center'>
-        <LineChart data={totalUsers3.getTotalUsersByJobPosition} dataKey='totalCount' xAxisKeys='jobPosition'></LineChart>
+        <LineChart data={totalUsersByJobPosition.getTotalUsersByJobPosition} dataKey='totalCount' xAxisKeys='jobPosition'></LineChart>
       </div>
       <div className='flex justify-center mt-12'>
-        <RadarChart data={totalUsers3.getTotalUsersByJobPosition}></RadarChart>
+        <RadarChart data={totalUsersByJobPosition.getTotalUsersByJobPosition}></RadarChart>
       </div>
     </div>);
 };
