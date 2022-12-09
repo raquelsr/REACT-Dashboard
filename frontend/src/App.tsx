fee7079825ba3c37dbd27042b94089786a4e0b5b
Raquel Serrano <raquelserranor@hotmail.com>
2022-12-09 16:02:41 +0100

    test code scanning

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { Header } from './components/Header';
 import { Dashboard } from './containers/Dashboard';
 
+console.log('error');
+
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <img src='https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/lm6ycpwrgyjqvpqaywj6'></img>
       <Dashboard />
     </div>
   );
